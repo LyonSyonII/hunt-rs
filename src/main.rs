69a3cd6fc8d34182c2aee4baa8520b29690e0db9
Liam G <liam.garriga@tutanota.com>
2022-07-01 17:27:17 +0200

    Added case sensitivity flag. The default search path is now the current directory. Simplified code.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ use std::{
     sync::atomic::AtomicBool, ops::Index,
 };
 
+#[derive(PartialEq, Clone, Copy)]
+enum Output {
+    Normal,
+    Simple,
+    SuperSimple,
+}
+
 enum FileType {
     Dir,
     File,
@@ -51,6 +58,12 @@ struct Cli {
     #[clap(short, long)]
     exact: bool,
 
+    /// If enabled, the search will be case-sensitive
+    /// 
+    /// Note that case-sensitivity will be activated automatically when the search query contains an uppercase letter.
+    #[clap(short, long)]
+    case_sensitive: bool,
+
     /// Print verbose output
     ///
     /// It'll show all errors found:    
@@ -69,10 +82,7 @@ struct Cli {
     /// If not enabled, hidden directories (starting with '.') and "/proc", "/root", "/boot", "/dev", "/lib", "/lib64", "/lost+found", "/run", "/sbin", "/sys", "/tmp", "/var/tmp", "/var/lib", "/var/log", "/var/db", "/var/cache", "/etc/pacman.d", "/etc/sudoers.d" and "/etc/audit" will be skipped
     #[clap(short, long)]
     hidden: bool,
-
-    // /// If enabled, hunt will not update the database if the file is not found
-    // // // #[clap(short, long="noupdate")]
-    // // no_update: bool,
+    
     /// Only files that start with this will be found
     #[clap(short = 'S', long = "starts")]
     starts_with: Option<String>,
@@ -99,12 +109,14 @@ struct Cli {
     /// Directories where you want to search
     ///
     /// If provided, hunt will only search there
+    /// 
+    /// If not provided, hunt will search in the current directory
     ///
     /// These directories are treated independently, so if one is nested into another the search will be done two times:
     ///
     /// e.g. "hunt somefile /home/user /home/user/downloads" will search in the home directory, and because /home/user/downloads is inside it, /downloads will be traversed two times
     #[clap(required = false)]
-    limit_to_dirs: Vec<PathBuf>,
+    search_in_dirs: Vec<PathBuf>,
 }
 
 fn parse_ignore_dirs(inp: &str) -> Result<HashSet<PathBuf>, String> {
@@ -119,9 +131,7 @@ struct Search<'a> {
     ends: &'a str,
     ftype: &'a FileType,
     current_dir: PathBuf,
-    home_dir: PathBuf,
-    root_dir: PathBuf,
-    ignore_paths: HashSet<&'static Path>
+    hardcoded_ignore: HashSet<&'static Path>
 }
 
 impl<'a> Search<'a> {
@@ -132,9 +142,7 @@ impl<'a> Search<'a> {
             ends, 
             ftype, 
             current_dir: std::env::current_dir().expect("Current directory could not be read"), 
-            home_dir: directories::UserDirs::new().expect("Home directory could not be read").home_dir().into(), 
-            root_dir: PathBuf::from("/"), 
-            ignore_paths: HashSet::from_iter(["/proc", "/root", "/boot", "/dev", "/lib", "/lib64", "/lost+found", "/run", "/sbin", "/sys", "/tmp", "/var/tmp", "/var/lib", "/var/log", "/var/db", "/var/cache", "/etc/pacman.d", "/etc/sudoers.d", "/etc/audit"].iter().map(Path::new)) 
+            hardcoded_ignore: HashSet::from_iter(["/proc", "/root", "/boot", "/dev", "/lib", "/lib64", "/lost+found", "/run", "/sbin", "/sys", "/tmp", "/var/tmp", "/var/lib", "/var/log", "/var/db", "/var/cache", "/etc/pacman.d", "/etc/sudoers.d", "/etc/audit"].iter().map(Path::new)) 
         }
     }
 }
@@ -186,10 +194,13 @@ fn search_dir(entry: std::fs::DirEntry, search: &Search, args: &Args, buffers: &
         entry.file_name().to_ascii_lowercase()
     };
     let fname = fname.to_string_lossy();
-
     let path = entry.path();
     
-    if !args.hidden && (fname.starts_with('.') || search.ignore_paths.contains(path.as_path())) || args.ignore.contains(&path) {
+    if args.ignore.contains(&path) {
+        return;
+    }
+
+    if !args.hidden && (fname.starts_with('.') || search.hardcoded_ignore.contains(path.as_path())) {
         return;
     }
 
@@ -200,15 +211,13 @@ fn search_dir(entry: std::fs::DirEntry, search: &Search, args: &Args, buffers: &
             is_dir
         }
         FileType::File => {
-            matches!(entry.file_type(), Ok(ftype) if ftype.is_file())
+            !is_dir
         }
         FileType::All => true,
     };
-    
 
     let starts = !search.starts.is_empty() || fname.starts_with(search.starts);
     let ends = !search.ends.is_empty() || fname.ends_with(search.ends);
-    
     if starts && ends && ftype {
         // If file name is equal to search name, write it to the "Exact" buffer
         if fname == search.name {
@@ -221,8 +230,7 @@ fn search_dir(entry: std::fs::DirEntry, search: &Search, args: &Args, buffers: &
     }
     
     // If entry is not a directory, stop function 
-    // Also skip CURRENT_DIR and HOME_DIR when limit is no specified, to avoid searching them twice
-    if !is_dir || (!args.limit && (path == search.current_dir || path == search.home_dir)) {
+    if !is_dir {
         return;
     }
     // If entry is a directory, search inside it
@@ -245,8 +253,8 @@ fn search_path(dir: &Path, search: &Search, args: &Args, buffers: &Buffers) {
     }
 }
 
-fn print_with_highlight(stdout: &mut std::io::StdoutLock, path: &Path, name: &str, simple: u8, case_sensitive: bool) -> std::io::Result<()> {
-    if simple == 0 {
+fn print_with_highlight(stdout: &mut std::io::StdoutLock, path: &Path, name: &str, simple: Output, case_sensitive: bool) -> std::io::Result<()> {
+    if simple == Output::Normal {
         let path = path.to_string_lossy();
         let search = if case_sensitive {
             path.to_string()
@@ -256,7 +264,7 @@ fn print_with_highlight(stdout: &mut std::io::StdoutLock, path: &Path, name: &st
 
         let start = search.rfind(name).unwrap();
         let end = start + name.len();
-        return writeln!(stdout, "{}{}{}", path.index(..start), path.index(start..end).bright_red(), path.index(end..));
+        return writeln!(stdout, "{}{}{}", path.index(..start), path.index(start..end).bright_red().bold(), path.index(end..));
     } 
         
     writeln!(stdout, "{}", path.display())
@@ -273,54 +281,41 @@ fn main() -> std::io::Result<()> {
         // If directory is given but no file name is specified, print files in that directory
         // ex. hunt /home/user
         Some(n) if n == "." || n.contains('/') => {
-            cli.limit_to_dirs.insert(0, PathBuf::from(n));
+            cli.search_in_dirs.insert(0, PathBuf::from(n));
             String::new()
         }
         Some(n) => n,
         None => String::new(),
     };
 
+    let simple = match cli.simple {
+        0 => Output::Normal,
+        1 => Output::Simple,
+        2.. => Output::SuperSimple,
+    };
+
     let search = Search::new(&name, &starts, &ends, &ftype);
 
-    let c_sensitive = name.contains(|c: char| c.is_alphabetic() && c.is_uppercase());
+    let c_sensitive = cli.case_sensitive || name.contains(|c: char| c.is_alphabetic() && c.is_uppercase());
     let ignore_dirs = cli.ignore_dirs.unwrap_or_default();
-
-    let buffers: Buffers = (Mutex::new(Vec::new()), Mutex::new(Vec::new()));
-
     let args = Args::new(
         cli.first,
         cli.exact,
-        !cli.limit_to_dirs.is_empty(),
+        !cli.search_in_dirs.is_empty(),
         cli.verbose,
         cli.hidden,
         &ignore_dirs,
         c_sensitive,
     );
-    
-    // If no limit, search current, home and root directories
-    if !args.limit {
-        let dirs = [
-            search.current_dir.as_path(),
-            search.home_dir.as_path(),
-            search.root_dir.as_path(),
-        ]
-        .into_iter();
 
-        // If only search for first, do it in order (less expensive to more)
-        if args.first {
-            for dir in dirs {
-                search_path(dir, &search, &args, &buffers);
-            }
-        }
-        // If search all occurrences, multithread search
-        else {
-            dirs.par_bridge().for_each(|dir| {
-                search_path(dir, &search, &args, &buffers);
-            });
-        }
+    let buffers: Buffers = (Mutex::new(Vec::new()), Mutex::new(Vec::new()));
+    
+    // If no limit, search current directory
+    if !args.limit {
+        search_path(&search.current_dir, &search, &args, &buffers);
     } else {
         // Check if paths are valid
-        let dirs = cli.limit_to_dirs.iter().map(|s| {
+        let dirs = cli.search_in_dirs.iter().map(|s| {
             std::fs::canonicalize(s).unwrap_or_else(|_| {
                 eprintln!("ERROR: The {:?} directory does not exist", s);
                 std::process::exit(1);
@@ -340,7 +335,7 @@ fn main() -> std::io::Result<()> {
         return Ok(());
     }
 
-    if cli.simple <= 1 {
+    if simple != Output::SuperSimple {
         co.par_sort_unstable();
         ex.par_sort_unstable();
     }
@@ -348,14 +343,13 @@ fn main() -> std::io::Result<()> {
     // Print results
     let mut stdout = std::io::stdout().lock();
     
-    if cli.simple == 0 {
+    if simple == Output::Normal {
         writeln!(stdout, "Contains:")?;
     }
-    //co.into_par_iter().for_each(op)
     for path in co {
-        print_with_highlight(&mut stdout, &path, search.name, cli.simple, args.case_sensitive)?;
+        print_with_highlight(&mut stdout, &path, search.name, simple, args.case_sensitive)?;
     }
-    if cli.simple == 0 { 
+    if simple == Output::Normal { 
         writeln!(stdout, "\nExact:")?; 
     }
     for path in ex {
