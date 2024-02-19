87ea71308d7d9cd832cac698bfce6c7b1aefadbd
lyonsyonii <liam@garriga.dev>
2024-02-19 23:02:29 +0100

    encapsulated par_fold

--- src/search.rs
+++ src/search.rs
@@ -28,54 +28,18 @@ impl Search {
                 }
             });
             // Search in directories
-            dirs.par_bridge()
-                .fold(
-                    || (Vec::new(), Vec::new()),
-                    |mut v, dir| {
-                        let results = search_path(dir.as_ref(), self);
-                        v.0.extend(results.0);
-                        v.1.extend(results.1);
-                        v
-                    },
-                )
-                .reduce(
-                    || (Vec::new(), Vec::new()),
-                    |mut acc, v| {
-                        acc.0.extend(v.0);
-                        acc.1.extend(v.1);
-                        acc
-                    },
-                )
+            par_fold(dirs, |dir| search_path(dir.as_ref(), self))
         }
     }
 }
 
 fn search_path(dir: &Path, search: &Search) -> Buffers {
     if let Ok(read) = std::fs::read_dir(dir) {
-        return read
-            .flatten()
-            .par_bridge()
-            .fold(
-                || (Vec::new(), Vec::new()),
-                |mut v, entry| {
-                    let results = search_dir(entry, search);
-                    v.0.extend(results.0);
-                    v.1.extend(results.1);
-                    v
-                },
-            )
-            .reduce(
-                || (Vec::new(), Vec::new()),
-                |mut acc, v| {
-                    acc.0.extend(v.0);
-                    acc.1.extend(v.1);
-                    acc
-                },
-            );
+        return par_fold(read.flatten(), |entry| search_dir(entry, search));
     } else if search.verbose {
         eprintln!("Could not read {:?}", dir);
     }
-    (Vec::new(), Vec::new())
+    new_buffers()
 }
 
 fn search_dir(entry: std::fs::DirEntry, search: &Search) -> Buffers {
@@ -89,7 +53,7 @@ fn search_dir(entry: std::fs::DirEntry, search: &Search) -> Buffers {
     let path = entry.path();
 
     if search.explicit_ignore.binary_search(&path).is_ok() {
-        return (Vec::new(), Vec::new());
+        return new_buffers();
     }
 
     let hardcoded = || {
@@ -100,7 +64,7 @@ fn search_dir(entry: std::fs::DirEntry, search: &Search) -> Buffers {
     };
 
     if !search.hidden && (is_hidden(&entry) || hardcoded()) {
-        return (Vec::new(), Vec::new());
+        return new_buffers();
     }
 
     // Read type of file and check if it should be added to search results
@@ -110,10 +74,10 @@ fn search_dir(entry: std::fs::DirEntry, search: &Search) -> Buffers {
         FileType::File => !is_dir,
         FileType::All => true,
     };
-    
+
     let starts = search.starts.is_empty() || fname.starts_with(&search.starts);
     let ends = search.ends.is_empty() || fname.ends_with(&search.ends);
-    let mut buffers = (Vec::new(), Vec::new());
+    let mut buffers = new_buffers();
 
     if starts && ends && ftype {
         // If file name is equal to search name, write it to the "Exact" buffer
@@ -132,32 +96,46 @@ fn search_dir(entry: std::fs::DirEntry, search: &Search) -> Buffers {
     }
 
     if let Ok(read) = std::fs::read_dir(&path) {
-        let b = read
-            .flatten()
-            .par_bridge()
-            .fold(
-                || (Buffer::new(), Buffer::new()),
-                |mut v, entry| {
-                    let results = search_dir(entry, search);
-                    v.0.extend(results.0);
-                    v.1.extend(results.1);
-                    v
-                },
-            )
-            .reduce(
-                || (Vec::new(), Vec::new()),
-                |mut acc, v| {
-                    acc.0.extend(v.0);
-                    acc.1.extend(v.1);
-                    acc
-                },
-            );
+        let b = par_fold(read.flatten(), |entry| search_dir(entry, search));
+
         buffers.0.extend(b.0);
         buffers.1.extend(b.1);
     } else if search.verbose {
         eprintln!("Could not read {:?}", path);
     }
     buffers
+}
+
+fn new_buffers() -> Buffers {
+    (Buffer::new(), Buffer::new())
+}
+
+fn par_fold<I, F, T>(iter: I, map: F) -> Buffers
+where
+    I: IntoIterator<Item = T>,
+    <I as IntoIterator>::IntoIter: Send,
+    F: Fn(T) -> Buffers + Sync + Send,
+    T: Send,
+{
+    iter.into_iter()
+        .par_bridge()
+        .map(map)
+        .fold(
+            new_buffers,
+            |mut acc, results| {
+                acc.0.extend(results.0);
+                acc.1.extend(results.1);
+                acc
+            },
+        )
+        .reduce(
+            new_buffers,
+            |mut acc, v| {
+                acc.0.extend(v.0);
+                acc.1.extend(v.1);
+                acc
+            },
+        )
 }
 
 fn print_var(var: &mut Buffer, first: bool, path: &Path, output: Output) {
